7f6de7beae9bc6a7d17c17af08320b7ea7d703d0
Jakob Wagner <jakob.e.wagner@tum.de>
2022-03-11 16:40:51 +0100

    add visualization dir and files

---
+++ src/visualization/console.h
@@ -0,0 +1,11 @@
+#ifndef SUDOKU_CONSOLE_H
+#define SUDOKU_CONSOLE_H
+
+#include "visualizer.h"
+
+class Console : public Visualizer {
+
+};
+
+
+#endif //SUDOKU_CONSOLE_H
---
+++ src/visualization/visualizer.h
@@ -0,0 +1,10 @@
+#ifndef SUDOKU_VISUALIZER_H
+#define SUDOKU_VISUALIZER_H
+
+
+class Visualizer {
+
+};
+
+
+#endif //SUDOKU_VISUALIZER_H
